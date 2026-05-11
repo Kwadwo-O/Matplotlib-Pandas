6c0082dd201e5c7113b4112cfa1ecbf83cad1ae5
S250217 <s2502817>
2026-05-11 10:01:54 +0100

    change

--- Matplotlib.py
+++ Matplotlib.py
@@ -1,5 +1,18 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv["file.csv"]
-print(df)
+df = pd.read_csv('file.csv')
+
+issue = input("put the athlete ID? ")
+
+issue_counts = df[df['Athlete ID']==issue]
+
+plt.bar(issue_counts.index, issue_counts.values, color='#2E75B6')
+plt.title('Number of Issues by Type', fontweight='bold')
+plt.xlabel('Issue Type')
+plt.ylabel('Count')
+plt.xticks(rotation=15)
+
+
+plt.tight_layout()
+plt.show()
